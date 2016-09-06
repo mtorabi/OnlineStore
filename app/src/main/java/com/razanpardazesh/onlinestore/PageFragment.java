559342ee8d788dccc40b5a31dab69fbe01943dd9
Torabi <torabi.mohammad@gmail.com>
2016-09-06 14:21:52 +0430

    first commit

---
+++ app/src/main/java/com/razanpardazesh/onlinestore/PageFragment.java
@@ -0,0 +1,55 @@
+package com.razanpardazesh.onlinestore;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Torabi on 9/6/2016.
+ */
+
+public class PageFragment extends Fragment {
+    private static final String ARG_PAGE_NUMBER = "page_number";
+
+    public PageFragment() {
+    }
+
+    public static PageFragment newInstance(int page) {
+        PageFragment fragment = new PageFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE_NUMBER, page);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_page_layout, container, false);
+
+        TextView txt = (TextView) rootView.findViewById(R.id.page_number_label);
+        int page = getArguments().getInt(ARG_PAGE_NUMBER, -1);
+
+        String temp = "";
+
+        switch (page)
+        {
+            case 1:
+                temp = "ویترین";
+                break;
+            case 2:
+                temp =  "لیست کالاها";
+                break;
+            case 3:
+                temp =  "لیست خرید";
+                break;
+        }
+
+        txt.setText(temp);
+
+        return rootView;
+    }
+}
