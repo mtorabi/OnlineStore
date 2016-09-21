d58e4ca3270a14a51298c3c078813f4432d06e06
MTG <torabi.mohammad@gmail.com>
2016-09-21 09:13:07 +0330

    second commit - library

---
+++ mtglibrary/src/main/java/com/razanpardazesh/mtglibrary/tools/NetworkAsyncWrapper.java
@@ -0,0 +1,17 @@
+package com.razanpardazesh.mtglibrary.tools;
+
+import android.content.Context;
+
+/**
+ * Created by Torabi on 9/13/2016.
+ */
+
+public class NetworkAsyncWrapper extends AsyncWrapper {
+
+
+    @Override
+    public void run(Context context, Object... params) {
+        //TODO mtg checkInternetIsAvailable
+        super.run(context, params);
+    }
+}
